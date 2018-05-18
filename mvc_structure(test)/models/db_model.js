1fb8fb31db972036e3a98a9678eb044234286f9b
YarZarTun <yarzartun.sead@gmail.com>
2018-05-18 12:19:42 +0630

    Delete db_model.js

--- mvc_structure(test)/models/db_model.js
+++
@@ -1,9 +0,0 @@
-var model = function() {
-	db = require('../config/db_helper'); 
-};
-model.prototype.execute = function(sql,callback){
-	db.query(sql,function(err,rows){
-		  		callback(err, rows);
-	});
-};
-module.exports = new model();
